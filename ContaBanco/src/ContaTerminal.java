6d97243645a5f5dcab749e215675f58c55fb97eb
marcelo-zagon <nikazagon@gmail.com>
2023-06-13 13:09:14 -0300

    Correção do campo nome

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 import java.io.IOException;
+ import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) throws IOException {
-        int numero;
+        int conta;
         String agencia;
         String nomeCliente; 
         double saldo;
-
+ 
+        Locale.setDefault(new Locale("en", "US"));
         Scanner entra = new Scanner(System.in);
  
         System.out.println("Por favor, digite o número da agência ! \n");
-        System.out.println("Número :");
-        numero = entra.nextInt();
+        System.out.println("Conta :");
+        conta = entra.nextInt();
 
         System.out.println("Agência: ");
         agencia = entra.next();
 
+        System.out.println("Nome Cliente : " );
+        nomeCliente = new Scanner(System.in).nextLine();  
+   
+
         System.out.println("Saldo:  ");
         saldo = entra.nextDouble();
-
-        System.out.println("Nome Cliente :" );
-        nomeCliente = entra.next();
+   
+  
+    System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s,  conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, conta, saldo );
  
- System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s,  conta %d e seu saldo %.2f  já está disponível para saque.", nomeCliente, agencia, numero, saldo );
  
  entra.close();
 
